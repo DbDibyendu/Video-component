433bb8203b67fb102816a2d1deec34f1efb8c32d
Dibyendu <dbdibyendu5@gmail.com>
2020-09-01 18:53:48 +0530

    edited

--- test.cpp
+++ test.cpp
@@ -1,58 +1,191 @@
 #include <iostream>
-#include <cstdlib>
+#include <stdio.h>
+#include <stdlib.h>
+#include <linux/ioctl.h>
+#include <linux/types.h>
+#include <linux/v4l2-common.h>
+#include <linux/v4l2-controls.h>
+#include <linux/videodev2.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <string.h>
+#include <fstream>
+#include <string>
 #include <tsanalyser/tsanalyser.h>
-#include <opencv2/opencv.hpp>
 
 using namespace std;
-using namespace cv;
 
-int main()
-{
 
-    /* Start Keeping time before the Key Task */
-    startKeepingTime();
+struct Initialise{
+    int fd;
+    v4l2_buffer bufferinfo;
+    v4l2_buffer queryBuffer;
+     v4l2_format imageFormat;
 
-    //initialize and allocate memory to load the video stream from camera 
-    cv::VideoCapture camera0(0);
-  //  cv::VideoCapture camera1(1);
+};
 
-    if( !camera0.isOpened() ){
-        cout<<"Error in opening Camera 0"<<endl;
-        return -1;
-    } 
-   // if( !camera1.isOpened() ) return -1;
+int Capture_Image(struct Initialise device){
 
-  //  while(true) {
-        //grab and retrieve each frames of the video sequentially 
-        cv::Mat3b frame0;
-        camera0 >> frame0;
-        
 
-     //   cv::imshow("Video0", frame0);
 
-        //wait for 40 milliseconds
-    //    int c = cvWaitKey(40);
+    // 1.  Open the device
 
-        //exit the loop if user press "Esc" key  (ASCII value of "Esc" is 27) 
-       // if(27 == char(c)) break;
- //   }
-
-    // Get the frame
-    double fps = camera0.get(CAP_PROP_FPS); 
-    cout << "Frames per seconds : " << fps << endl;
-    Mat save_img; camera0 >> save_img;
-    if(save_img.empty())
-    {
-        std::cerr << "Something is wrong with the webcam, could not get frame." << std::endl;
+    device.fd = open("/dev/video0",O_RDWR);
+    if(device.fd < 0){
+        perror("Failed to open device, OPEN");
+        return 1;
     }
 
-    // Save the frame into a file
-    imwrite("test1.jpg", save_img); // A JPG FILE IS BEING SAVED
+        // 2. Ask the device if it can capture frames
+    v4l2_capability capability;
+    if(ioctl(device.fd, VIDIOC_QUERYCAP, &capability) < 0){
+        // something went wrong... exit
+        perror("Failed to get device capabilities, VIDIOC_QUERYCAP");
+        return 1;
+    }
 
-       getTimeTaken(DEBUG_MODE);
+    // 3. Set Image format
+   
+    device.imageFormat.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    device.imageFormat.fmt.pix.width = 1024;
+    device.imageFormat.fmt.pix.height = 1024;
+    device.imageFormat.fmt.pix.pixelformat = V4L2_PIX_FMT_MJPEG;
+    device.imageFormat.fmt.pix.field = V4L2_FIELD_NONE;
+    
+    // tell the device you are using this format
+    if(ioctl(device.fd, VIDIOC_S_FMT, &device.imageFormat) < 0){
+        perror("Device could not set format, VIDIOC_S_FMT");
+        return 1;
+    }
 
-        /*Print total time taken and total RAM usage*/
-       getTSAnlaysis(DEBUG_MODE);
+     // 4. Request Buffers from the device
+    v4l2_requestbuffers requestBuffer = {0};
+    requestBuffer.count = 1; // one request buffer
+    requestBuffer.type = V4L2_BUF_TYPE_VIDEO_CAPTURE; // request a buffer wich we an use for capturing frames
+    requestBuffer.memory = V4L2_MEMORY_MMAP;
+
+    if(ioctl(device.fd, VIDIOC_REQBUFS, &requestBuffer) < 0){
+        perror("Could not request buffer from device, VIDIOC_REQBUFS");
+        return 1;
+    }
+
+
+    // 5. Quety the buffer to get raw data ie. ask for the you requested buffer
+    // and allocate memory for it
+   // v4l2_buffer queryBuffer = {0};
+    device.queryBuffer.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    device.queryBuffer.memory = V4L2_MEMORY_MMAP;
+    device.queryBuffer.index = 0;
+    if(ioctl(device.fd, VIDIOC_QUERYBUF, &device.queryBuffer) < 0){
+        perror("Device did not return the buffer information, VIDIOC_QUERYBUF");
+        return 1;
+    }
+    // use a pointer to point to the newly created buffer
+    // mmap() will map the memory address of the device to
+    // an address in memory
+    char* buffer = (char*)mmap(NULL, device.queryBuffer.length, PROT_READ | PROT_WRITE, MAP_SHARED,
+                        device.fd, device.queryBuffer.m.offset);
+    memset(buffer, 0, device.queryBuffer.length);
+
+    // 6. Get a frame
+    // Create a new buffer type so the device knows whichbuffer we are talking about
+
+
+    memset(&device.bufferinfo, 0, sizeof(device.bufferinfo));
+    device.bufferinfo.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    device.bufferinfo.memory = V4L2_MEMORY_MMAP;
+    device.bufferinfo.index = 0;
+
+    // Activate streaming
+    int type = device.bufferinfo.type;
+    if(ioctl(device.fd, VIDIOC_STREAMON, &type) < 0){
+        perror("Could not start streaming, VIDIOC_STREAMON");
+        return 1;
+    }
+
+/***************************** Begin looping here *********************/
+    // Queue the buffer
+    if(ioctl(device.fd, VIDIOC_QBUF, &device.bufferinfo) < 0){
+        perror("Could not queue buffer, VIDIOC_QBUF");
+        return 1;
+    }
+
+    // Dequeue the buffer
+    if(ioctl(device.fd, VIDIOC_DQBUF, &device.bufferinfo) < 0){
+        perror("Could not dequeue the buffer, VIDIOC_DQBUF");
+        return 1;
+    }
+    // Frames get written after dequeuing the buffer
+
+    cout << "Buffer has: " << (double)device.bufferinfo.bytesused / 1024
+            << " KBytes of data" << endl;
+
+    // Write the data out to file
+    v4l2_buffer bufferinfo;
+    ofstream outFile;
+    outFile.open("webcam_output.jpeg", ios::binary| ios::app);
+
+    int bufPos = 0, outFileMemBlockSize = 0;  // the position in the buffer and the amoun to copy from
+                                        // the buffer
+    int remainingBufferSize = device.bufferinfo.bytesused; // the remaining buffer size, is decremented by
+                                                    // memBlockSize amount on each loop so we do not overwrite the buffer
+    char* outFileMemBlock = NULL;  // a pointer to a new memory block
+    int itr = 0; // counts thenumber of iterations
+    while(remainingBufferSize > 0) {
+        bufPos += outFileMemBlockSize;  // increment the buffer pointer on each loop
+                                        // initialise bufPos before outFileMemBlockSize so we can start
+                                        // at the begining of the buffer
+
+        outFileMemBlockSize = 1024;    // set the output block size to a preferable size. 1024 :)
+        outFileMemBlock = new char[sizeof(char) * outFileMemBlockSize];
+
+        char* buffer = (char*)mmap(NULL, device.queryBuffer.length, PROT_READ | PROT_WRITE, MAP_SHARED,
+                        device.fd, device.queryBuffer.m.offset);
+        // copy 1024 bytes of data starting from buffer+bufPos
+        memcpy(outFileMemBlock, buffer+bufPos, outFileMemBlockSize);
+        outFile.write(outFileMemBlock,outFileMemBlockSize);
+
+        // calculate the amount of memory left to read
+        // if the memory block size is greater than the remaining
+        // amount of data we have to copy
+        if(outFileMemBlockSize > remainingBufferSize)
+            outFileMemBlockSize = remainingBufferSize;
+
+        // subtract the amount of data we have to copy
+        // from the remaining buffer size
+        remainingBufferSize -= outFileMemBlockSize;
+
+        // display the remaining buffer size
+     //   cout << itr++ << " Remaining bytes: "<< remainingBufferSize << endl;
+        
+        delete outFileMemBlock;
+
+         }
+
+    // Close the file
+    outFile.close();
+
+
+    // end streaming
+     if(ioctl(device.fd, VIDIOC_STREAMOFF, &type) < 0){
+         perror("Could not end streaming, VIDIOC_STREAMOFF");
+          return 1;
+          }
+
+    close(device.fd);
+
+}
+
+
+
+int main() {
+
+    struct Initialise device1;
+
+    Capture_Image(device1);  
+    
 
     return 0;
 }
